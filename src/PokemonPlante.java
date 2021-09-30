ba39b8c043bb19304df5f7fe607f017206a2eadc
Your Name <you@example.com>
2021-09-30 14:26:10 +0200

    creation de la methode PokemonPlante

--- src/PokemonPlante.java
+++ src/PokemonPlante.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class PokemonPlante {
+public class PokemonPlante extends Pokemon{
+      public PokemonPlante(String nom,int hp, int atk){
+      super (nom,hp,atk);
+
+      }
+
+      @Override
+    public void attacker (Pokemon p) {
+          if (p = PokemonEau) {
+              p.hp = atk * 2 - p.hp;
+          }
+          else if(Pokemonfeu){
+              p.hp = atk * 0.5 - p.hp;
+
+          }
+
+          else
+              p.hp = atk - p.hp ;
+
+      }
+
 }
+
+
+
+
+
