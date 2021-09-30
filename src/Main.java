7490d8dfd291bfd42f1cfebe5f5dd16762055027
- <theobreton@yahoo.frgit config -- global user.name Theo>
2021-09-30 22:12:01 +0100

    Test main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ public class Main {
 
     public static void main(String[] args){
 
+    PokemonEau e = new PokemonEau("Squirtle",44,48);
+    PokemonPlante b = new PokemonPlante("Bulbasaur",45,49);
+    Pokemon p = new Pokemonfeu("Charmander",39,52);
+
+    e.getNom();
+    e.gethp();
+    p.getNom();
+    p.gethp();
 
     }
 }
